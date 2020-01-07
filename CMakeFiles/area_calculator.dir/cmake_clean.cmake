955702f74bfd29966a68b3182714455fe173e477
JShweiri <wafflemanz3000@gmail.com>
2020-01-07 10:09:27 -0800

    added?

---
+++ CMakeFiles/area_calculator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/area_calculator.dir/src/main.cpp.o"
+  "CMakeFiles/area_calculator.dir/src/rectangle.cpp.o"
+  "area_calculator.pdb"
+  "area_calculator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/area_calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/bigger_area_calculator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bigger_area_calculator.dir/src/new_main.cpp.o"
+  "CMakeFiles/bigger_area_calculator.dir/src/rectangle.cpp.o"
+  "bigger_area_calculator.pdb"
+  "bigger_area_calculator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bigger_area_calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
